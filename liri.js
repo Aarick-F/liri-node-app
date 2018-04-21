06dfcf27cafa9505f8edf08bc9ece86c9a307d35
Aarick-F <zeflyndesign@gmail.com>
2018-04-21 01:16:29 -0400

    Added the rest of the functionality

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ const Spotify = require("node-spotify-api");
 const request = require("request");
 const inquirer = require("inquirer");
 const keys = require("./keys.js");
+const fs = require("file-system");
 
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
@@ -35,6 +36,8 @@ function startApp() {
           }
         ]).then(data => {
           spotifyThis(data.songChoice);
+        }).catch(err => {
+          spotifyThis("The Ace");
         });
         break;
       case "movie-this":
@@ -70,6 +73,7 @@ function myTweets() {
       console.log(err);
     } else {
       tweets.forEach(tweet => {
+        console.log("DATE CREATED: " + tweet.created_at);
         console.log(tweet.text);
         console.log("====================================");
       });
@@ -88,15 +92,42 @@ function spotifyThis(song) {
       console.log(err);
     } else {
       let artist = data.tracks.items[0].artists[0].name;
+      let album = data.tracks.items[0].album.name;
+      let preview = data.tracks.items[0].preview_url;
+      console.log("SONG: " + song);
       console.log("ARTIST: " + artist);
+      console.log("ALBUM: " + album);
+      console.log("PREVIEW: " + preview);
     }
+    startApp();
   });
 }
 
 function movieThis(movie) {
-  console.log("YOU'VE MADE IT TO THE MOVIE FUNCTION, YOU'RE SONG CHOICE WAS: " + movie);
+  let key = "&apikey=8fe5fd42";
+  let query = "http://www.omdbapi.com/?t=" + movie + key;
+  request(query, function(err, res, body) {
+    if (!err && res.statusCode === 200) {
+      let data = JSON.parse(body);
+      console.log("TITLE: " + data.Title);
+      console.log("YEAR: " + data.Year);
+      console.log("IMDB RATING: " + data.Ratings[0].Value);
+      console.log("ROTTEN TOMATOES RATING: " + data.Ratings[1].Value);
+      console.log("COUNTRY: " + data.Country);
+      console.log("LANGUAGE: " + data.Language);
+      console.log("PLOT: " + data.Plot);
+      console.log("ACTORS: " + data.Actors);
+    }
+    startApp();
+  });
 }
 
 function doThis() {
-
+  fs.readFile("random.txt", "UTF-8", function(err, data) {
+    if(err) {
+      console.log(err);
+    } else {
+      spotifyThis(data);
+    }
+  });
 }
